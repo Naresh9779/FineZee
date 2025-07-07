93bf4e2bebfe5fcf4cb697685ea4e88f12d043a3
NARESH THAPA <115917313+Naresh9779@users.noreply.github.com>
2025-07-07 11:10:10 +0000

    done

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,12 @@
 const express =require("express")
 const authController=require("../controller/authController")
 const studentController=require("../controller/studentController")
+const factory=require("../controller/factoryController")
 const router=express.Router();
 
-router.get('/get_students',authController.protect,authController.restrictTo('admin'),studentController.getAllStudents)
+router.get('/get_students',authController.protect,authController.restrictTo('admin'),studentController.getAllStudents);
+router.delete('/delete_student/:id',authController.protect,authController.restrictTo('admin'),factory.deleteOne);
+router.get('/get_student/:id',authController.protect,authController.restrictTo('admin'),factory.getOne);
+router.patch('/update_student/:id',authController.protect,authController.restrictTo('admin'),factory.updateOne);
 
 module.exports=router;
--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -8,27 +8,26 @@ const router=express.Router();
 
 
 router.post('/signup',authController.signUp);
+
 router.post('/login',authController.login);
+
 router.get('/logout',authController.logout);
 
 // router.post('/forgotPassword',authController.forgotPassword);
 // router.patch('/resetPassword/:token',authController.resetPassword);
 
-//details
+//Profile
 router.get('/get_profile',authController.protect,studentController.getMe,studentController.getStudent);
-
-
-
-
-//update profile
 
 router.patch('/update_profile',authController.protect,studentController.updateStudent);
 
 router.patch('/update_password',authController.protect,authController.updatePassword);
 
 
+
 //BOOKMARK
 router.get('/get_bookmarked_universities',authController.protect,studentController.getBookmarkedUniversities);
+
 router.post('/toggle_bookmark_university',authController.protect,studentController.toggleBookmarkUniversity);
 
 module.exports=router;
