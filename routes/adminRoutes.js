fe9dda7c152daa43fa164b8829f1b44669b792cc
NARESH THAPA <115917313+Naresh9779@users.noreply.github.com>
2025-07-07 11:22:10 +0000

    Student Admin Route Update

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,11 +2,12 @@ const express =require("express")
 const authController=require("../controller/authController")
 const studentController=require("../controller/studentController")
 const factory=require("../controller/factoryController")
+const Student=require("../models/studentModel")
 const router=express.Router();
 
 router.get('/get_students',authController.protect,authController.restrictTo('admin'),studentController.getAllStudents);
-router.delete('/delete_student/:id',authController.protect,authController.restrictTo('admin'),factory.deleteOne);
-router.get('/get_student/:id',authController.protect,authController.restrictTo('admin'),factory.getOne);
-router.patch('/update_student/:id',authController.protect,authController.restrictTo('admin'),factory.updateOne);
+router.delete('/delete_student/:id',authController.protect,authController.restrictTo('admin'),factory.deleteOne(Student));
+router.get('/get_student/:id',authController.protect,authController.restrictTo('admin'),factory.getOne(Student));
+router.patch('/update_student/:id',authController.protect,authController.restrictTo('admin'),factory.updateOne(Student));
 
 module.exports=router;
