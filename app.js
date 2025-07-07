1f40661ebaa6eb83c29817d32915f343b654966f
NARESH THAPA <115917313+Naresh9779@users.noreply.github.com>
2025-07-07 07:06:28 +0000

    new Update

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use(express.json());
 
 app.use(cookieParser())
 
+app.get('/', (req, res) => {
+  res.send('🚀 Fine-Zee API is live');
+});
+
 app.use('/api/university',university);
 app.use('/api/student',student)
 app.use('/api/admin',admin)
